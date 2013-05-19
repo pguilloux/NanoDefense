bd4ed48bf39de49dabd5f1f01dc61ac71b6c77b8
pguilloux <pguilloux53@gmail.com>
2013-05-20 00:45:47 +0200

    switch du démarrage en mode éditeur ou en mode jeu

--- src/Run.java
+++ src/Run.java
@@ -2,7 +2,9 @@ public class Run
 {	
 	public static void main(String[] args)
 	{
-				MapEditor game = new MapEditor("save.txt");
+				//MapEditor game = new MapEditor("save.txt");
 				//game.build();
+				Dispatcher game = new Dispatcher();
+
 	}
 }
