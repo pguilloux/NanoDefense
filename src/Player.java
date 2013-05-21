6ae19d0517b810e57b9d44a68ea4a1c5428969a8
flavienINK <flavien.leygues@gmail.com>
2013-05-21 18:15:37 +0200

    TEST premier commit : première version de la classe Player avec constructeurs, getters et setters.

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,58 @@ import java.util.ArrayList;
 public class Player
 {
 	private int money;
-	private ArrayList<Zone> zones;
-	private ArrayList<Tower> towers;	
+	private ArrayList<Zone> zoneList;
+	private ArrayList<Tower> towerList;
+	
+	/* ----------------------------- */
+	/*   Déclaration des méthodes    */
+	/* ----------------------------- */
+	
+	public Player() {
+		this.money = 0;
+		this.zoneList = new ArrayList<Zone>();
+		this.towerList = new ArrayList<Tower>();
+	}
+	
+	public Player(int MONEY, ArrayList<Zone> ZONELIST, ArrayList<Tower> TOWERLIST) {
+		this.money = MONEY;
+		this.zoneList = ZONELIST;
+		this.towerList = TOWERLIST;
+	}
+	
+	/* ----------------------------- */
+	/*    Déclaration des GETERS     */
+	/* ----------------------------- */
+	
+	public int getMoney() {
+		return this.money;
+	}
+	
+	public ArrayList<Zone> getZoneList() {
+		return this.zoneList;
+	}
+	
+	public ArrayList<Tower> getTowerList() {
+		return this.towerList;
+	}
+	
+	/* ----------------------------- */
+	/*    Déclaration des SETERS     */
+	/* ----------------------------- */
+	
+	public void setMoney(int MONEY) {
+		this.money = MONEY;
+	}
+	
+	public void setZoneList(ArrayList<Zone> ZONELIST) {
+		this.zoneList = ZONELIST;
+	}
+	
+	public void setTowerList(ArrayList<Tower> TOWERLIST) {
+		this.towerList = TOWERLIST;
+	}
+	
 }
+
+
+
