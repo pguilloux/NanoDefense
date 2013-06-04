24ebd89cebf754d09738e22bf4ed3448574dddd5
j.carly <jcarly@netemedia.fr>
2013-06-05 01:02:44 +0200

    mod speed+, life+ et dédoublement implémentés

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -139,14 +139,16 @@ class GameEngine extends JFrame implements Runnable
 				{
 					pan.remove(agents.get(i));
 					//agents.get(i).setText(String.valueOf(nb_agents));
-					if(!agents.get(i).getMove())
+					if(agents.get(i).getLife()<=0 && agents.get(i).getMove())
 					{
 						if(agents.get(i).getMod(2))
 							for(int k=0; k<4; k++)
-								agents.add(new Agent(agents.get(i).getProprio(),agents.get(i).getx(),agents.get(i).gety()+k,agents.get(i).getZoneStart(),agents.get(i).getZoneStop(),map));
+								agents.add(new Agent(agents.get(i).getProprio(),agents.get(i).getx(),agents.get(i).gety()+k,agents.get(i).getPath(),agents.get(i).getZoneStart(),agents.get(i).getZoneStop(),map));
+						agents.get(i).setMove(false);
 						agents.remove(agents.get(i));
 					}
-					
+					if(!agents.get(i).getMove())
+						agents.remove(agents.get(i));
 					else
 					{
 						agents.get(i).move();							
